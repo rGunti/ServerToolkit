3abe6d0208fa490c3e07534f5fd4c156daffa6a1
Raphael Guntersweiler <raphael@rgunti.ch>
2019-08-03 12:49:57 +0200

    Added logging and updating

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Updates the git repository to the newest version and makes all scripts executable
+SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "${SCRIPT_LOCATION}/utils/log.sh"
+
+cd "$SCRIPT_LOCATION"
+
+logVerbose "Pulling updates ..."
+git pull
+
+if [ $? -ne 0 ]; then
+    logFatal "Failed to update scripts!"
+    exit 1
+fi
+
+logVerbose "Making scripts executable ..."
+chmod +x **/*.sh
+
+logInfo "Update completed!"
--- utils/log.sh
+++ utils/log.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-function echoErr {
-    >&2 echo $1
+function log {
+    _icon=$1
+    _msg=$2
+    echo "$(date +'%F %T') $1 $2"
 }
+
+function logInternal { log " ^ " "$2"; }
+function logVerbose { log "   " "$2"; }
+function logDebug { log " * " "$2"; }
+function logInfo { log "[i]" "$2"; }
+function logWarn { log "[!]" "$2"; }
+function logError { log "/!\\" "$2"; }
+function logFatal { >&2 log "!!!" "$2"; }
+
+function echoErr { >&2 echo $1; }
+
+logInternal "Included utils"
