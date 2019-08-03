4c4ba13ffa0354e5407ef9f376dcc35fa4fcec69
Raphael Guntersweiler <raphael@rgunti.ch>
2019-08-03 13:28:01 +0200

    Added user check

--- gameserver/minecraft/setup.sh
+++ gameserver/minecraft/setup.sh
@@ -3,4 +3,22 @@ SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "${SCRIPT_LOCATION}/../../utils/functions.sh"
 
+# ### Functions ###
+function ensureGameserver {
+    _gameserverUser=${1:-gameserver}
+
+    userInfo=$(cat /etc/passwd | grep ${_gameserverUser})
+    if [ $? -ne 1 ]; then
+        logFatal "User \"${_gameserverUser}\" could not be found!"
+        exit 1
+    fi
+
+    IFS=':' read -ra ADDR <<< "${userInfo}"
+    export GAMESERVER_HOMEDIR="${ADDR[5]}"
+}
+
+# ### main() ###
 ensureRoot
+ensureGameserver "gameserver"
+
+echo "Gameserver Home Directory: ${GAMESERVER_HOMEDIR}"
